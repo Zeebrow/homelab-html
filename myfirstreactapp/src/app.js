e46f3c5e1d2ad3bf6eb0eeb1f9e160cbcad6d428
Mike Zborowski <mzborowski@yahoo.com>
2020-01-31 21:05:15 -0500

    Fixes

--- myfirstreactapp/src/app.js
+++ myfirstreactapp/src/app.js
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom';
 import MyList from './components/mylist.js';
 import MyButton from './components/mybutton/mybutton.js';
 
+let leButton = new MyButton;
+console.log(leButton);
 class App extends React.Component {
     state = { value: 0 };
-    leButton = new MyButton;
-    console.log(leButton);
     render() {
         return(
-            <div>
-                { leButton }
+            <div>Yo.
             </div>
         );
 
