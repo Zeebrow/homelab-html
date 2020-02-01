01684481113cc401b42f5bd4dd3e357ee646aed0
Mike Zborowski <mzborowski@carelogistics.com>
2020-01-31 20:08:21 -0500

    update

---
+++ myfirstreactapp/src/app.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MyList from './components/mylist.js';
+import MyButton from './components/mybutton.js';
+
+class App extends React.Component {
+    state = { value: 0 };
+
+    render() {
+        return(
+            <div>Hello, World!</div>
+        );
+
+
+    }
+
+}
+
+export default App;
