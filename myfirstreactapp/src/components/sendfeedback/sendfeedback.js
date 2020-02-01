b44f94592338f79ce2a659fbcfe090ba1e537616
Mike Zborowski <mzborowski@carelogistics.com>
2020-02-01 00:51:02 -0500

    update

--- myfirstreactapp/src/components/sendfeedback/sendfeedback.js
+++ myfirstreactapp/src/components/sendfeedback/sendfeedback.js
@@ -5,16 +5,14 @@ class SendFeedback extends React.Component {
         super(props);
 		this.state = {
             server: '192.168.1.76',
-            data: 'test-data'
         };
-
         this.sendFeedback = this.sendFeedback.bind(this);
     }
 
-    receiveData() {
-        console.log("Inside receiveData(). Data: " + this.props.data;
-        this.setState({data: this.props.data});
-    }
+    // receiveData() {
+    //     console.log("Inside receiveData(). Data: " + this.props.data;
+    //     this.setState({data: this.props.data});
+    // }
 
 	sendFeedback = () => {
         console.log("Inside sendFeedback(). Data: " + this.props.data;
@@ -23,13 +21,13 @@ class SendFeedback extends React.Component {
 		this.setState({ server: '192.168.1.76' });
         alert('Sending data to ' + this.state.server);
 
-        console.log("Data: "+this.state.data);
+        console.log("Data: "+this.props.data);
         console.log("Server: "+this.state.server);
         var xhr = new XMLHttpRequest();
-        xhr.open("POST", '', true);
+        xhr.open("POST", this.props.data, true);
         xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.send(JSON.stringify({
-            data: this.state.data
+            data: this.props.data
         }));
         console.log('Sending data to ' + this.state.server);
 
